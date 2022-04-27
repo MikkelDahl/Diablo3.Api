6f6703694d69bfa18fd09b0ad552cc85a7f2d5fe
Mikkel <mikkeldahlpoulsen@gmail.com>
2022-04-27 20:41:00 +0200

    Adding default configuration

---
+++ Diablo3.Api.Core/Models/DefaultClientConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Diablo3.Api.Core.Models;
+
+public class DefaultClientConfiguration : ClientConfiguration
+{
+    private DefaultClientConfiguration(CacheConfiguration cacheConfiguration) : base(cacheConfiguration)
+    {
+        
+    }
+
+    public static DefaultClientConfiguration GetConfiguration()
+    {
+        var cacheConfig = new CacheConfiguration(CacheOptions.Default);
+        return new DefaultClientConfiguration(cacheConfig);
+    }
+}
--- Diablo3.Prototype/Program.cs
+++ Diablo3.Prototype/Program.cs
@@ -2,7 +2,7 @@
 using Diablo3.Api.Core;
 using Diablo3.Api.Core.Models;
 
-var configuration = new ClientConfiguration(new CacheConfiguration(CacheOptions.Default, 2000));
+var configuration = DefaultClientConfiguration.GetConfiguration();
 var client = new DiabloClientFactory(Region.EU, "d0696d1b62c940d3ace557ddd33eed54", "j9Z13V9a5W555ltePARV15pgGh8am3Z1")
     .Build(configuration);
 
