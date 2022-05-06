d0ac695caca6b67e38843c67fbe59731a4322abe
Mikkel <mikkeldahlpoulsen@gmail.com>
2022-05-06 23:45:07 +0200

    Fixing broken unit tests

--- Diablo3.Api.Core.Test/DiabloClientTest.cs
+++ Diablo3.Api.Core.Test/DiabloClientTest.cs
@@ -5,6 +5,7 @@ using Diablo3.Api.Core.Models;
 using Diablo3.Api.Core.Services;
 using Moq;
 using NUnit.Framework;
+using Serilog;
 
 namespace Diablo3.Api.Core.Test
 {
@@ -13,6 +14,8 @@ namespace Diablo3.Api.Core.Test
     {
         private DiabloClient sut = null!;
         private Mock<ILeaderBoardFetcher> leaderBoardFetcherMock = null!;
+        private Mock<IBattleNetApiHttpClient> battleNetApiHttpClientMock = null!;
+        private Mock<ILogger> loggerMock = null!;
         private Mock<IHeroFetcher> heroFetcherMock = null!;
 
         [SetUp]
@@ -20,47 +23,45 @@ namespace Diablo3.Api.Core.Test
         {
             leaderBoardFetcherMock = new Mock<ILeaderBoardFetcher>();
             heroFetcherMock = new Mock<IHeroFetcher>();
-            sut = new DiabloClient(leaderBoardFetcherMock.Object, heroFetcherMock.Object);
-        
-            SetupDataFetcherMock();
-        }
+            battleNetApiHttpClientMock = new Mock<IBattleNetApiHttpClient>();
+            loggerMock = new Mock<ILogger>();
+            sut = new DiabloClient(heroFetcherMock.Object,
+                new ClientConfiguration(new CacheConfiguration(CacheOptions.Default)),
+                battleNetApiHttpClientMock.Object, loggerMock.Object, 1);
 
-        [Test]
-        public void Constructor_throws_on_null_args()
-        {
-            Assert.Throws<ArgumentNullException>(() => new DiabloClient(null, heroFetcherMock.Object));
-            Assert.Throws<ArgumentNullException>(() => new DiabloClient(leaderBoardFetcherMock.Object, null));
+            SetupDataFetcherMock();
         }
 
         [Test]
         public async Task GetForClassAsync_calls_fetcher_once()
         {
-            var _ = await sut.GetForClassAsync(HeroClass.Barbarian);
-            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>(), It.IsAny<bool>()), Times.Once);
+            var _ = await sut.GetLeaderBoardForClassAsync(HeroClass.Barbarian);
+            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>()),
+                Times.Once);
         }
-    
+
         [Test]
         public async Task GetAllAsync_calls_fetcher_once_per_class()
         {
             var _ = await sut.GetAllAsync();
-            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>(), It.IsAny<bool>()), Times.Exactly(7));
-
+            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>()),
+                Times.Exactly(7));
         }
-    
+
         [Test]
         public async Task GetAllHardcoreAsync_calls_fetcher_once_per_class()
         {
             var _ = await sut.GetAllHardcoreAsync();
-            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>(), It.IsAny<bool>()), Times.Exactly(7));
-
+            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>()),
+                Times.Exactly(7));
         }
-    
+
         [Test]
         public async Task GetHardcoreForClassAsync_calls_fetcher_with_hardcore_param_set_to_true()
         {
-            var _ = await sut.GetHardcoreForClassAsync(HeroClass.Barbarian);
-            leaderBoardFetcherMock.Verify(a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>(), It.Is<bool>(b => b == true)), Times.Once);
-
+            var _ = await sut.GetHardcoreLeaderBoardForClassAsync(HeroClass.Barbarian);
+            leaderBoardFetcherMock.Verify(
+                a => a.GetLeaderBoardAsync(It.IsAny<HeroClass>()), Times.Once);
         }
 
         private void SetupDataFetcherMock()
@@ -68,7 +69,7 @@ namespace Diablo3.Api.Core.Test
             var leaderBoardEntries = new List<LeaderBoardEntry>();
 
             leaderBoardFetcherMock.Setup(a =>
-                    a.GetLeaderBoardAsync(It.IsAny<HeroClass>(), It.IsAny<bool>()))
+                    a.GetLeaderBoardAsync(It.IsAny<HeroClass>()))
                 .ReturnsAsync(new LeaderBoard(leaderBoardEntries));
         }
     }
