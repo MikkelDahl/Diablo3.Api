5ebcf44d77d44cc8531846d4db6fcbda35143400
Mikkel <mikkeldahlpoulsen@gmail.com>
2022-05-06 09:31:10 +0200

    Cache WIP

---
+++ Diablo3.Api.Core/Models/Cache/ICache.cs
@@ -0,0 +1,6 @@
+﻿namespace Diablo3.Api.Core.Models.Cache;
+
+internal interface ICache<TKey, TValue>
+{
+    Task<TValue> GetAsync(TKey key);
+}
---
+++ Diablo3.Api.Core/Models/Cache/LeaderBoardCache.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Diablo3.Api.Core.Models.Cache;
+
+internal class LeaderBoardCache : ICache<CacheKey, LeaderBoard>
+{
+    private readonly IMemoryCache cache;
+
+    public LeaderBoardCache(IMemoryCache cache)
+    {
+        this.cache = cache ?? throw new ArgumentNullException(nameof(cache));
+    }
+
+    public async Task<LeaderBoard> GetAsync(CacheKey key)
+    {
+        if (cache.TryGetValue(key, out LeaderBoard value))
+            return value;
+
+        value = await fetcher();
+        cache.Set(key, value);
+        return value;
+    }
+
+    public async Task SetAsync()
+    {
+        
+    }
+}
