c6888e25cce71dfa920b69514a2cc01e463e75ad
Mikkel <mikkeldahlpoulsen@gmail.com>
2022-05-29 21:45:51 +0200

    Adding json objects for items

---
+++ Diablo3.Api.Core/Models/DTOs/AttributeDataObject.cs
@@ -0,0 +1,7 @@
+namespace Diablo3.Api.Core.Models.DTOs;
+
+public class AttributeDataObject
+{
+    public string textHtml { get; set; }
+    public string text { get; set; }
+}
---
+++ Diablo3.Api.Core/Models/DTOs/ItemAttributes.cs
@@ -0,0 +1,7 @@
+namespace Diablo3.Api.Core.Models.DTOs;
+
+public class ItemAttributes
+{
+    public List<AttributeDataObject> primary { get; set; }
+    public List<AttributeDataObject> secondary { get; set; }
+}
