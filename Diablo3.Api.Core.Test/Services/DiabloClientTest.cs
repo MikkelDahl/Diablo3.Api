f881bd72c660f24c341f3c5e4e096311ddebc285
Mikkel <mikkeldahlpoulsen@gmail.com>
2022-06-05 15:03:26 +0200

    Housekeeping

--- Diablo3.Api.Core.Test/Services/DiabloClientTest.cs
+++ Diablo3.Api.Core.Test/Services/DiabloClientTest.cs
@@ -1,35 +1,14 @@
-﻿using Diablo3.Api.Core.Services;
-using Diablo3.Api.Core.Services.Characters;
-using Moq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Diablo3.Api.Core.Test.Services
 {
     [TestFixture]
     public class DiabloClientTest
     {
-        private Mock<IHeroFetcher> heroFetcherMock;
-        private Mock<IAccountFetcher> accountFetcherMock;
-        private Mock<ILeaderBoardService> leaderBoardServiceMock;
-        private Mock<IItemFetcher> itemCacheMock;
-
-        [SetUp]
-        public void Setup()
-        {
-            leaderBoardServiceMock = new Mock<ILeaderBoardService>(); 
-            heroFetcherMock = new Mock<IHeroFetcher>();
-            itemCacheMock = new Mock<IItemFetcher>();
-            accountFetcherMock = new Mock<IAccountFetcher>();
-        }
-
         [Test]
         public void Constructor_throws_on_null_args()
         {
            TestHelper.AssertConstructorThrowsOnNullArgs<DiabloClient>();
         }
-
-
-        private DiabloClient Sut() =>
-            new(leaderBoardServiceMock.Object, heroFetcherMock.Object, itemCacheMock.Object, accountFetcherMock.Object);
     }
 }
